8139883bfe8320208766b744008ca1c7294e1325
shen2 <zhenyupku@gmail.com>
2014-08-30 16:38:42 +0800

    remove try-catch in Result::readByTypeFromStream()

--- Cassandra/Response/Result.php
+++ Cassandra/Response/Result.php
@@ -20,74 +20,70 @@ class Result extends DataStream{
 	 * @return mixed
 	 */
 	public function readByTypeFromStream(array $type){
-		try {
-			$length = unpack('N', substr($this->data, $this->offset, 4))[1];
-			$this->offset += 4;
-			
-			if ($this->length < $this->offset + $length)
-				return null;
-			
-			// do not use $this->read() for performance
-			$data = substr($this->data, $this->offset, $length);
-			$this->offset += $length;
-			
-			switch ($type['type']) {
-				case DataTypeEnum::ASCII:
-				case DataTypeEnum::VARCHAR:
-				case DataTypeEnum::TEXT:
-					return $data;
-				case DataTypeEnum::BIGINT:
-				case DataTypeEnum::COUNTER:
-				case DataTypeEnum::VARINT:
-					$unpacked = unpack('N2', $data);
-					return $unpacked[1] << 32 | $unpacked[2];
-				case DataTypeEnum::CUSTOM:
-				case DataTypeEnum::BLOB:
-					$length = unpack('N', substr($data, 0, 4))[1];
-					if ($length == 4294967295 || $length + 4 > strlen($data))
-						return null;
-					return substr($data, 4, $length);
-				case DataTypeEnum::BOOLEAN:
-					return (bool) unpack('C', $data)[1];
-				case DataTypeEnum::DECIMAL:
-					$unpacked = unpack('N3', $data);
-					$value = $unpacked[2] << 32 | $unpacked[3];
-					$len = strlen($value);
-					return substr($value, 0, $len - $unpacked[1]) . '.' . substr($value, $len - $unpacked[1]);
-				case DataTypeEnum::DOUBLE:
-					return unpack('d', strrev($data))[1];
-				case DataTypeEnum::FLOAT:
-					return unpack('f', strrev($data))[1];
-				case DataTypeEnum::INT:
-					return unpack('N', $data)[1];
-				case DataTypeEnum::TIMESTAMP:
-					$unpacked = unpack('N2', $data);
-					return round($unpacked[1] * 4294967.296 + ($unpacked[2] / 1000));
-				case DataTypeEnum::UUID:
-				case DataTypeEnum::TIMEUUID:
-					$uuid = '';
-					for ($i = 0; $i < 16; ++$i) {
-						if ($i == 4 || $i == 6 || $i == 8 || $i == 10) {
-							$uuid .= '-';
-						}
-						$uuid .= str_pad(dechex(ord($data{$i})), 2, '0', STR_PAD_LEFT);
-					}
-					return $uuid;
-				case DataTypeEnum::INET:
-					return inet_ntop($data);
-				case DataTypeEnum::COLLECTION_LIST:
-				case DataTypeEnum::COLLECTION_SET:
-					$dataStream = new DataStream($data);
-					return $dataStream->readList($type['value']);
-				case DataTypeEnum::COLLECTION_MAP:
-					$dataStream = new DataStream($data);
-					return $dataStream->readMap($type['key'], $type['value']);
-				default:
-					trigger_error('Unknown type ' . var_export($type, true));
-					return null;
-			}
-		} catch (\Exception $e) {
+		$length = unpack('N', substr($this->data, $this->offset, 4))[1];
+		$this->offset += 4;
+		
+		if ($this->length < $this->offset + $length)
 			return null;
+		
+		// do not use $this->read() for performance
+		$data = substr($this->data, $this->offset, $length);
+		$this->offset += $length;
+		
+		switch ($type['type']) {
+			case DataTypeEnum::ASCII:
+			case DataTypeEnum::VARCHAR:
+			case DataTypeEnum::TEXT:
+				return $data;
+			case DataTypeEnum::BIGINT:
+			case DataTypeEnum::COUNTER:
+			case DataTypeEnum::VARINT:
+				$unpacked = unpack('N2', $data);
+				return $unpacked[1] << 32 | $unpacked[2];
+			case DataTypeEnum::CUSTOM:
+			case DataTypeEnum::BLOB:
+				$length = unpack('N', substr($data, 0, 4))[1];
+				if ($length == 4294967295 || $length + 4 > strlen($data))
+					return null;
+				return substr($data, 4, $length);
+			case DataTypeEnum::BOOLEAN:
+				return (bool) unpack('C', $data)[1];
+			case DataTypeEnum::DECIMAL:
+				$unpacked = unpack('N3', $data);
+				$value = $unpacked[2] << 32 | $unpacked[3];
+				$len = strlen($value);
+				return substr($value, 0, $len - $unpacked[1]) . '.' . substr($value, $len - $unpacked[1]);
+			case DataTypeEnum::DOUBLE:
+				return unpack('d', strrev($data))[1];
+			case DataTypeEnum::FLOAT:
+				return unpack('f', strrev($data))[1];
+			case DataTypeEnum::INT:
+				return unpack('N', $data)[1];
+			case DataTypeEnum::TIMESTAMP:
+				$unpacked = unpack('N2', $data);
+				return round($unpacked[1] * 4294967.296 + ($unpacked[2] / 1000));
+			case DataTypeEnum::UUID:
+			case DataTypeEnum::TIMEUUID:
+				$uuid = '';
+				for ($i = 0; $i < 16; ++$i) {
+					if ($i == 4 || $i == 6 || $i == 8 || $i == 10) {
+						$uuid .= '-';
+					}
+					$uuid .= str_pad(dechex(ord($data{$i})), 2, '0', STR_PAD_LEFT);
+				}
+				return $uuid;
+			case DataTypeEnum::INET:
+				return inet_ntop($data);
+			case DataTypeEnum::COLLECTION_LIST:
+			case DataTypeEnum::COLLECTION_SET:
+				$dataStream = new DataStream($data);
+				return $dataStream->readList($type['value']);
+			case DataTypeEnum::COLLECTION_MAP:
+				$dataStream = new DataStream($data);
+				return $dataStream->readMap($type['key'], $type['value']);
+			default:
+				trigger_error('Unknown type ' . var_export($type, true));
+				return null;
 		}
 	}
 	
